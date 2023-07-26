4a58c0fe3006ab4cdbe24ddb88f8f61873f94ecf
LadyDammy <oreoluwadammie@gmail.com>
2023-07-26 02:48:21 -0700

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-"""
+"""Hypermedia pagination sample.
 """
 import csv
 import math
-from typing import List, Dict
+from typing import Dict, List, Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    # """returns index range for items"""
-    return (((page - 1) * page_size), page * page_size)
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -17,6 +20,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -31,32 +36,28 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """returns a dataset for a given datarange"""
-        try:
-            assert (page > 0 or page_size > 0)
-            assert (type(page) is int or type(page_size) is int)
-            pages = index_range(page, page_size)
-            dataset = self.dataset()[pages[0]:pages[1]]
-            return dataset
-        except Exception:
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
+        return data[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """returns a dict of meta data"""
-        data = self.get_page(page, page_size)
-        if len(self.dataset()) % page_size == 0:
-            total_pages = len(self.dataset()) / page_size
-        else:
-            total_pages = len(self.dataset()) / page_size + 1
-        next_page = None if (page + 1) > total_pages else page + 1
-        prev_page = None if page - 1 < 0 else page - 1
-
-        my_dict = {
+        """Retrieves information about a page.
+        """
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
             'page': page,
-            'page_size': page_size,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': int(total_pages)
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
         }
-        return my_dict
+        return page_info
